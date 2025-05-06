9e3a9d124b8f4de6619eda97327291e601a6ea21
prasadbarhate8080 <prasadbarhate388@gmail.com>
2025-05-06 13:37:32 +0530

    testing

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@ import express, { urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 const app = express();
+const allowedOrigins  = process.env.CORS_ORIGIN?.split(",") || [];
 
-app.use(cors(
-    {
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    }
-));
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  };
+
+app.use(cors(corsOptions))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true,limit: "16kb"}));
--- src/controllers/train.controller.js
+++ src/controllers/train.controller.js
@@ -6,7 +6,7 @@ import { trainModel } from "../models/train.model.js";
 const getTrains = asyncHandler(async (req, res) => {
 
     const {source, destination} = req.query;
-    // console.log(source,destination);
+    console.log(source,destination);
 
     if(!source || !destination)
     {
